fb17646c0fa555b45fe446fb34d41a3c0970cc06
IsaacH <Isaac.hathaway28@gmail.com>
2022-02-02 15:51:37 -0700

    add 3x cached elemt references for input, button, and ul

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('DOM To-Do Lab');
+
+const inp = document.querySelector('#typed-message')
+console.log(inp)
+
+const btn = document.querySelector('#submit-button')
+
+console.log(btn)
+
+const toDoList = document.getElementById('todo-list') 
+// .getElementById is used just to practice different cached element references
+console.log(toDoList)
